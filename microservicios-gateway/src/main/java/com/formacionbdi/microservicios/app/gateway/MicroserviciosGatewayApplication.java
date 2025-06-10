d0c06fad17336d4506d42dfcde498160b1f62da0
acairamp <alancairampoma@gmail.com>
2025-06-10 01:42:49 -0500

    Restore OAuth2 2.1 SecurityConfig

--- microservicios-gateway/src/main/java/com/formacionbdi/microservicios/app/gateway/MicroserviciosGatewayApplication.java
+++ microservicios-gateway/src/main/java/com/formacionbdi/microservicios/app/gateway/MicroserviciosGatewayApplication.java
@@ -2,10 +2,11 @@ package com.formacionbdi.microservicios.app.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroserviciosGatewayApplication {
 
 	public static void main(String[] args) {
