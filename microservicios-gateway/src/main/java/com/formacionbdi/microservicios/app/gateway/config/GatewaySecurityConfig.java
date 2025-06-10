d52e2e3c179a6a38b7b2f0a402b74d18f30cbea6
acairamp <alancairampoma@gmail.com>
2025-06-10 00:49:49 -0500

    claude deploy root gateway 4 doker file

--- microservicios-gateway/src/main/java/com/formacionbdi/microservicios/app/gateway/config/GatewaySecurityConfig.java
+++ microservicios-gateway/src/main/java/com/formacionbdi/microservicios/app/gateway/config/GatewaySecurityConfig.java
@@ -5,12 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
+@Order(1)
 public class GatewaySecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
